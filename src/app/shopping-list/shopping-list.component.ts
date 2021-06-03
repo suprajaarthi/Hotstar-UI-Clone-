7c0a466936aa4bb22342d9dc2339225b877ec6cc
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-03 02:00:21 -0700

    Add files via upload

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscribable, Subscription } from 'rxjs';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [];
+  private subscription : Subscription;
+
+
+  constructor(private slService : ShoppingListService) { }
+
+  ngOnInit() {
+    this.ingredients = this.slService.getIngredients()
+    this.subscription=this.slService.ingredientsChanged
+    .subscribe(
+      (ingredients: Ingredient[] )=>
+  {       this.ingredients=ingredients;}
+      );
+  }
+  onEditItem(index : number)
+  {
+    this.slService.startedEditing.next(index);
+  }
+  ngOnDestroy(): void 
+  {
+    this.subscription.unsubscribe();
+  }
+
+}
